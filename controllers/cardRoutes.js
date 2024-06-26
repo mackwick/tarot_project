b45bd390ec59fe76d53d44102fe657d901cfd5a2
mackwick <mackenziewicker@gmail.com>
2024-06-26 15:39:38 -0400

    delete route works

--- controllers/cardRoutes.js
+++ controllers/cardRoutes.js
@@ -27,6 +27,15 @@ router.post("/", async (req, res) => {
 });
 
 //DESTROY
+router.delete("/:id", async (req, res) => {
+  try {
+    const byeCard = await Card.findByIdAndDelete(req.params.id);
+    res.json(byeCard);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json(err);
+  }
+});
 
 //UPDATE
 
